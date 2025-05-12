7e0a3c5ebefcdb89988b6130911dd0d6a2794e83
João Pedro Leal <jpgloob@gmail.com>
2025-05-11 23:00:52 -0300

    Atualização

--- utillib.c
+++ utillib.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "utillib.h"
 
 void desentgl(int altura, char caractere, int largura, Fortrian tipo, Tipodes tipo2){
@@ -67,7 +66,7 @@ void desenqdd(int lado, char caractere, Tipodes tipo){
     while(altFazendo <= lado){
         for(int i = 0; i < lado && tipo == PRE; i++)
             printf("%c%c", caractere, caractere);
-            for(int i = 0; i < lado && tipo == ESP; i++)
+        for(int i = 0; i < lado && tipo == ESP; i++)
             printf("%c ", caractere);
         printf("\n");
         altFazendo++;
@@ -79,9 +78,157 @@ void desenrtg(int altura, char caractere, int largura, Tipodes tipo){
     while(altFazendo <= altura){
         for(int i = 0; i < largura && tipo == PRE; i++)
             printf("%c%c", caractere, caractere);
-            for(int i = 0; i < largura && tipo == ESP; i++)
+        for(int i = 0; i < largura && tipo == ESP; i++)
             printf("%c ", caractere);
         printf("\n");
         altFazendo++;
+    }
+}
+
+void remesp(char *str){
+    int len = strlen(str);
+    int j = 0;
+    for(int i = 0; i < len; i++)
+        if(str[i] != ' ')
+            str[j++] = str[i];
+    str[j] = '\0';
+}
+
+void strtolower(char *str){
+    int len = strlen(str);
+    for(int i = 0; i < len; i++)
+        str[i] = tolower(str[i]);
+}
+
+void strtoupper(char *str){
+    int len = strlen(str);
+    for(int i = 0; i < len; i++)
+        str[i] = toupper(str[i]);
+}
+
+char* tobase(Dado dado, Tp_dado tp_dado, int base, int tamanho){
+    if(base <= 1 || base > 200)
+        return NULL;
+    else if(tp_dado == TP_INT || tp_dado == TP_CHAR){
+        int num = (tp_dado == TP_INT ? dado.i : dado.c);
+
+        if(tamanho <= 0){
+            char montagem[100];
+            if(num == 0)
+                montagem[0] = '0';
+            for(int i = 0; num != 0; i++){
+                int resto = num % base;
+                num /= base;
+                montagem[i] = ((resto - 10) < 0 ? resto + '0' : (resto - 10) + 'A');
+                if(num == 0)
+                    montagem[i + 1] = '\0';
+            }
+
+            int tam = strlen(montagem);
+            char *resultado = malloc((tam + 1) * sizeof(char));
+
+            for (int i = tam - 1, j = 0; i >= 0; i--, j++){
+                resultado[j] = montagem[i];
+            }
+            resultado[tam] = '\0';
+            return resultado;
+        }
+        else{
+            char montagem[tamanho + 1];
+            for(int i = 0; i < tamanho; i++){
+                if(num != 0){
+                    int resto = num % base;
+                    num /= base;
+                    montagem[i] = ((resto - 10) < 0 ? resto + '0' : (resto - 10) + 'A');
+                }
+                else
+                    montagem[i] = '0';
+            }
+            montagem[tamanho] = '\0';
+
+            char *resultado = malloc((tamanho + 1) * sizeof(char));
+
+            for (int i = tamanho - 1, j = 0; i >= 0; i--, j++){
+                resultado[j] = montagem[i];
+            }
+            resultado[tamanho] = '\0';
+            return resultado;
+        }
+    }
+    else if(tp_dado == TP_STRING){
+        if ((tamanho <= 0 && (base == 2 || base == 16)) || tamanho > 0){
+
+            if (tamanho <= 0 && base == 2)
+                tamanho = 8;
+            else if (tamanho <= 0 && base == 16)
+                tamanho = 2;
+
+            int tamtotal = (tamanho + 1) * (strlen(dado.s));
+
+            char montagem[tamanho + 1];
+            char *resultado = malloc(tamtotal * sizeof(char));
+
+            int len = strlen(dado.s);
+            int j = 0;
+            for(int letra = 0; letra < len; letra++){
+                int num = dado.s[letra];
+
+                for(int i = 0; i < tamanho; i++){
+                    if(num != 0){
+                        int resto = num % base;
+                        num /= base;
+                        montagem[i] = ((resto - 10) < 0 ? resto + '0' : (resto - 10) + 'A');
+                    }
+                    else
+                        montagem[i] = '0';
+                }
+                montagem[tamanho] = '\0';
+
+
+                for (int i = tamanho - 1; i >= 0; i--, j++){
+                    resultado[j] = montagem[i];
+                }
+                if((letra + 1) < len)
+                    resultado[j++] = ' ';
+                else{
+                    resultado[j] = '\0';
+                    return resultado;
+                }
+            }
+        }
+        else if (tamanho <= 0){
+            int tamtotal = 11 * (strlen(dado.s));
+
+            char montagem[tamtotal];
+            char *resultado = malloc(tamtotal * sizeof(char));
+
+            int len = strlen(dado.s);
+            int j = 0;
+            for(int letra = 0; letra < len; letra++){
+                int num = dado.s[letra];
+
+                for(int i = 0; num != 0; i++){
+                    int resto = num % base;
+                    num /= base;
+                    montagem[i] = ((resto - 10) < 0 ? resto + '0' : (resto - 10) + 'A');
+                    if(num == 0)
+                        montagem[i + 1] = '\0';
+                }
+
+                int tam = strlen(montagem);
+                for (int i = tam - 1; i >= 0; i--, j++){
+                    resultado[j] = montagem[i];
+                }
+                if((letra + 1) < len)
+                    resultado[j++] = ' ';
+                else{
+                    resultado[j] = '\0';
+                    return resultado;
+                }
+            }
+        }
+    }
+    else{
+        return NULL;
     }
 }
--- utillib.h
+++ utillib.h
@@ -1,6 +1,12 @@
 #ifndef UTILLIB_H_INCLUDED
 #define UTILLIB_H_INCLUDED
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
 typedef enum {
     ISOS,
     RETDIR,
@@ -12,12 +18,40 @@ typedef enum {
     ESP
 } Tipodes;
 
+typedef union {
+    char *s;
+    char c;
+    int i;
+} Dado;
+
+typedef enum {
+    TP_STRING,
+    TP_CHAR,
+    TP_INT
+} Tp_dado;
+
+//10/05/2025
 void desentgl(int altura, char caractere, int largura, Fortrian tipo, Tipodes tipo2);
 
+//10/05/2025
 void desenlsg(int altura, char caractere, int largura, Tipodes tipo);
 
+//10/05/2025
 void desenqdd(int lado, char caractere, Tipodes tipo);
 
+//10/05/2025
 void desenrtg(int altura, char caractere, int largura, Tipodes tipo);
+
+//10/05/2025
+void remesp(char *str);
+
+//10/05/2025
+void strtolower(char *str);
+
+//10/05/2025
+void strtoupper(char *str);
+
+//10/05/2025
+char* tobase(Dado dado, Tp_dado tp_dado, int base, int tamanho);
 
 #endif // UTILLIB_H_INCLUDED
